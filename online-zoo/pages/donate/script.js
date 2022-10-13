15fe095c1e3bc82f1ba7c6f417e432c9585d6f2e
Ulia Skripka <minilektrina@gmail.com>
2022-10-13 18:04:19 +0300

    feat: added burger icon

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -1,1 +1,8 @@
-"!"
+const iconMenu = document.querySelector('.burger_icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu_body');
+    iconMenu.addEventListener("click", function (e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,1 +1,10 @@
-alert('Work in progress! Give me some time, please =) | Работа в процессе! Позволь мне доделать, пожалуйста =)')
+// alert('Work in progress! Give me some time, please =) | Работа в процессе! Позволь мне доделать, пожалуйста =)')
+
+const iconMenu = document.querySelector('.burger_icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu_body');
+    iconMenu.addEventListener("click", function (e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
